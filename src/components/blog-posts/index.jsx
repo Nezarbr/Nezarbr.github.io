090a4ab457ba14b2d8ac577fe51925dff473015a
NEZAR <nezarbo001@gmail.com>
2024-10-17 17:10:27 +0200

    Co

--- src/components/blog-posts/index.jsx
+++ src/components/blog-posts/index.jsx
@@ -7,13 +7,15 @@ const BlogPosts = ({ posts }) => {
   return (
     <Section title="All Blog Posts">
       {posts.map((post) => (
-        <SummaryItem
-          key={post.node.fields.slug}
-          name={post.node.frontmatter.title}
-          description={post.node.frontmatter.description}
-          link={post.node.fields.slug}
-          internal
-        />
+        <div key={post.node.fields.slug} className="mb-4">
+          <SummaryItem
+            name={post.node.frontmatter.title}
+            description={post.node.frontmatter.description}
+            link={post.node.fields.slug}
+            internal
+          />
+          <p className="text-sm text-gray-500">Click on the title to access the full post.</p>
+        </div>
       ))}
     </Section>
   );
